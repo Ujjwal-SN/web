55f1fd31739ef4de0fee88d74c91668ed47ad574
Ujjwal Singh <raghuvanshiujjwalsingh11@gmail.com>
2026-06-04 18:04:47 +0530

    .html to php

--- index.php
+++ index.php
@@ -1,28 +1,26 @@
 <?php include 'layout/header.php'; ?>
- 
- 
  <nav class="navbar" id="navbar" aria-label="Main navigation">
         <div class="container nav-inner">
-            <a href="index.html" class="logo">
+            <a href="index.php" class="logo">
              <img src="images/newnexlogo.webp" alt="Nexus Property Logo" class="logo-img" width="120" height="56">
             </a>
 <ul class="nav-menu" id="navMenu">
-    <li><a href="index.html">Home</a></li>
+    <li><a href="index.php">Home</a></li>
     <li class="dropdown">
-        <a href="about.html" class="dropdown-toggle">About Us</a>
+        <a href="about.php" class="dropdown-toggle">About Us</a>
         <ul class="dropdown-menu">
-            <li><a href="what-we-do.html">What We Do</a></li>
-            <li><a href="managing-director.html">Managing Director</a></li>
-            <li><a href="ceo.html">CEO</a></li>
+            <li><a href="what-we-do.php">What We Do</a></li>
+            <li><a href="managing-director.php">Managing Director</a></li>
+            <li><a href="ceo.php">CEO</a></li>
         </ul>
     </li>
-    <li><a href="team.html">Our Team</a></li>
-    <li><a href="properties.html">Property</a></li>
-    <li><a href="blog.html">Blog</a></li>
-    <li><a href="gallery.html">Gallery &amp; Videos</a></li>
-    <li><a href="contact.html">Contact Us</a></li>
+    <li><a href="team.php">Our Team</a></li>
+    <li><a href="properties.php">Property</a></li>
+    <li><a href="blog.php">Blog</a></li>
+    <li><a href="gallery.php">Gallery &amp; Videos</a></li>
+    <li><a href="contact.php">Contact Us</a></li>
 </ul>
-            <a href="contact.html" class="btn-cta">Book Site Visit</a>
+            <a href="contact.php" class="btn-cta">Book Site Visit</a>
             <button class="hamburger" id="hamburger" aria-label="Open navigation menu" aria-expanded="false" aria-controls="navMenu">
                 <span></span><span></span><span></span>
             </button>
