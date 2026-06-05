d7943f2f21ef0e251e8418122effaa2ab54cc13e
Ujjwal Singh <raghuvanshiujjwalsingh11@gmail.com>
2026-06-05 16:01:37 +0530

    update footer.php

--- layout/footer.php
+++ layout/footer.php
@@ -1,4 +1,4 @@
- <!-- Footer -->
+php <!-- Footer -->
     <footer class="footer">
         <div class="container footer-inner">
             <div class="footer-col">
@@ -15,11 +15,11 @@
             <div class="footer-col">
                 <h4>Quick Links</h4>
                 <ul>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="about.html">About Us</a></li>
-                    <li><a href="properties.html">Properties</a></li>
-                    <li><a href="team.html">Our Team</a></li>
-                    <li><a href="contact.html">Contact Us</a></li>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="properties.php">Properties</a></li>
+                    <li><a href="team.php">Our Team</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
                 </ul>
             </div>
             <div class="footer-col">
